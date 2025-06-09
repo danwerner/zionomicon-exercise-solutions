2e08720d24cf2f6d772472e1a641a9c17791aa22
Daniel Werner <daniel.d.werner@gmail.com>
2025-06-10 00:40:24 +0200

    Add zio-streams for Chapter 11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,19 +5,25 @@ object Dependencies {
   object ZIO {
     val zioVersion = "2.1.19"
 
-    val zio = "dev.zio" %% "zio" % zioVersion
+    val zio        = "dev.zio" %% "zio"         % zioVersion
+    val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
   }
 
   object Testing {
-    val zioTest = "dev.zio" %% "zio-test" % ZIO.zioVersion
+    val zioTest         = "dev.zio" %% "zio-test"          % ZIO.zioVersion
+    val zioTestSbt      = "dev.zio" %% "zio-test-sbt"      % ZIO.zioVersion
+    val zioTestMagnolia = "dev.zio" %% "zio-test-magnolia" % ZIO.zioVersion
   }
 
   val dependencies: List[ModuleID] = List(
-    ZIO.zio
+    ZIO.zio,
+    ZIO.zioStreams
   )
 
   val testDependencies: List[ModuleID] = List(
-    Testing.zioTest
+    Testing.zioTest,
+    Testing.zioTestSbt,
+    Testing.zioTestMagnolia
   )
 
 }
